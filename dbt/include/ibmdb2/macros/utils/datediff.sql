e251b15c9754a8a7d734101339194da546de2c69
aurany <rasmus.nyberg@gmail.com>
2022-09-08 19:50:12 +0200

    Tests implemented for all existing utility macros

--- dbt/include/ibmdb2/macros/utils/datediff.sql
+++ dbt/include/ibmdb2/macros/utils/datediff.sql
@@ -4,7 +4,7 @@
     {#
         DB2 dateparts:
         --------------
-        1 : Fractions of a second
+        1 : Microseconds
         2 : Seconds
         4 : Minutes
         8 : Hours
@@ -31,13 +31,19 @@
         {% set _datepart = 4 %}
     {% elif datepart == 'second' %}
         {% set _datepart = 2 %}
+    {% elif datepart == 'microsecond' %}
+        {% set _datepart = 1 %}
     {% else %}
         {{ exceptions.raise_compiler_error("Unsupported datepart for macro datediff in ibmdb2: {!r}".format(datepart)) }}
     {% endif %}
 
-    timestampdiff(
-        {{ _datepart }},
-        char(cast('{{ second_date }}' as datetime) - cast('{{ first_date }}' as datetime))
-        )
+    case when {{ second_date }} is null or {{ first_date }} is null
+      then null
+      else
+      timestampdiff(
+          {{ _datepart }},
+          char(cast({{ second_date }} as timestamp) - cast({{ first_date }} as timestamp))
+      )
+    end
 
 {% endmacro %}
--- tests/functional/adapter/test_macros.py
+++ tests/functional/adapter/test_macros.py
@@ -104,76 +104,184 @@ seeds:
         result_3: varchar(10)
 """
 
-# class TestHashIBMDB2(BaseHash):
-#     @pytest.fixture(scope="class")
-#     def seeds(self):
-#         return {
-#             "data_hash.csv": seeds__data_hash_csv,
-#             "data_hash.yml": seeds__schema_hash_yml,
-#         }
-#
-# class TestPositionIBMDB2(BasePosition):
-#     @pytest.fixture(scope="class")
-#     def seeds(self):
-#         return {
-#             "data_position.csv": seeds__data_position_csv,
-#             "data_position.yml": seeds__schema_position_yml,
-#         }
-#
-# class TestLastDayIBMDB2(BaseLastDay):
-#     @pytest.fixture(scope="class")
-#     def seeds(self):
-#         return {
-#             "data_last_day.csv": seeds__data_last_day_csv,
-#             "data_last_day.yml": seeds__schema_last_day_yml,
-#         }
+seeds__data_dateadd_csv = """from_time,interval_length,datepart,result
+2018-01-01 01:00:00,1,day,2018-01-02 01:00:00
+2018-01-01 01:00:00,1,month,2018-02-01 01:00:00
+2018-01-01 01:00:00,1,year,2019-01-01 01:00:00
+2018-01-01 01:00:00,1,hour,2018-01-01 02:00:00
+,1,day,
+"""
 
-# class TestTypeStringIBMDB2(BaseTypeString):
-#     @pytest.fixture(scope="class")
-#     def seeds(self):
-#         return {
-#             "expected.csv": seeds__data_type_string_csv,
-#             "expected.yml": seeds__schema_type_string_csv,
-#         }
-#
-#     @pytest.fixture(scope="class")
-#     def models(self):
-#         return {"actual.sql": self.interpolate_macro_namespace(models__type_string_actual_sql, "type_string")}
+seeds__schema_dateadd_yml = """
+version: 2
+seeds:
+  - name: data_dateadd
+    config:
+      quote_columns: false
+      column_types:
+        from_time: datetime
+        interval_length: int
+        datepart: varchar(10)
+        result: datetime
+"""
 
-# class TestSplitPartIBMDB2(BaseSplitPart):
-#     @pytest.fixture(scope="class")
-#     def seeds(self):
-#         return {
-#             "data_split_part.csv": seeds__data_split_part_csv,
-#             "data_split_part.yml": seeds__schema_split_part_yml,
-#         }
+models__test_dateadd_sql = """
+with data as (
+    select * from {{ ref('data_dateadd') }}
+)
+select
+    case
+        when datepart = 'hour' then cast({{ dateadd('hour', 'interval_length', 'from_time') }} as {{ api.Column.translate_type('timestamp') }})
+        when datepart = 'day' then cast({{ dateadd('day', 'interval_length', 'from_time') }} as {{ api.Column.translate_type('timestamp') }})
+        when datepart = 'month' then cast({{ dateadd('month', 'interval_length', 'from_time') }} as {{ api.Column.translate_type('timestamp') }})
+        when datepart = 'year' then cast({{ dateadd('year', 'interval_length', 'from_time') }} as {{ api.Column.translate_type('timestamp') }})
+        else null
+    end as actual,
+    result as expected
+from data
+"""
 
-#class TestDateAdd(BaseDateAdd):
-    #pass
-    # @pytest.fixture(scope="class")
-    # def seeds(self):
-    #     return {"data_dateadd.csv": seeds__data_dateadd_csv}
-    #
-    # @pytest.fixture(scope="class")
-    # def models(self):
-    #     return {
-    #         "test_dateadd.yml": models__test_dateadd_yml,
-    #         "test_dateadd.sql": self.interpolate_macro_namespace(
-    #             models__test_dateadd_sql, "dateadd"
-    #         ),
-    #     }
+seeds__data_datediff_csv = """first_date,second_date,datepart,result
+2018-01-01 01:00:00,2018-01-02 01:00:00,day,1
+2018-01-01 01:00:00,2018-02-01 01:00:00,month,1
+2018-01-01 01:00:00,2019-01-01 01:00:00,year,1
+2018-01-01 01:00:00,2018-01-01 02:00:00,hour,1
+2018-01-01 01:00:00,2018-01-01 02:01:00,minute,61
+2018-01-01 01:00:00,2018-01-01 02:00:01,second,3601
+2019-12-31 00:00:00,2019-12-30 00:00:00,week,0
+2019-12-31 00:00:00,2020-01-02 00:00:00,week,0
+2019-12-31 00:00:00,2020-01-08 02:00:00,week,1
+2019-12-31 00:00:00,2019-12-22 00:00:00,week,-1
+,2018-01-01 02:00:00,hour,
+2018-01-01 02:00:00,,hour,
+"""
 
-#class TestDateDiff(BaseDateDiff):
-    #pass
-    # @pytest.fixture(scope="class")
-    # def seeds(self):
-    #     return {"data_datediff.csv": seeds__data_datediff_csv}
-    #
-    # @pytest.fixture(scope="class")
-    # def models(self):
-    #     return {
-    #         "test_datediff.yml": models__test_datediff_yml,
-    #         "test_datediff.sql": self.interpolate_macro_namespace(
-    #             models__test_datediff_sql, "datediff"
-    #         ),
-    #     }
+seeds__schema_datediff_yml = """
+version: 2
+seeds:
+  - name: data_datediff
+    config:
+      quote_columns: false
+      column_types:
+        first_date: datetime
+        second_date: datetime
+        datepart: varchar(10)
+        result: int
+"""
+
+models__test_datediff_sql = """
+with data as (
+    select * from {{ ref('data_datediff') }}
+)
+select
+    case
+        when datepart = 'second' then {{ datediff('FIRST_DATE', 'SECOND_DATE', 'second') }}
+        when datepart = 'minute' then {{ datediff('FIRST_DATE', 'SECOND_DATE', 'minute') }}
+        when datepart = 'hour' then {{ datediff('FIRST_DATE', 'SECOND_DATE', 'hour') }}
+        when datepart = 'day' then {{ datediff('FIRST_DATE', 'SECOND_DATE', 'day') }}
+        when datepart = 'week' then {{ datediff('FIRST_DATE', 'SECOND_DATE', 'week') }}
+        when datepart = 'month' then {{ datediff('FIRST_DATE', 'SECOND_DATE', 'month') }}
+        when datepart = 'year' then {{ datediff('FIRST_DATE', 'SECOND_DATE', 'year') }}
+        else null
+    end as actual,
+    result as expected
+from data
+-- Also test correct casting of literal values.
+union all select {{ datediff("'1999-12-31 23:59:59.999999'", "'2000-01-01 00:00:00.000000'", "microsecond") }} as actual, 1 as expected from sysibm.sysdummy1
+union all select {{ datediff("'1999-12-31 23:59:59.000000'", "'2000-01-01 00:00:00.000000'", "second") }} as actual, 1 as expected from sysibm.sysdummy1
+union all select {{ datediff("'1999-12-31 23:59:00.000000'", "'2000-01-01 00:00:00.000000'", "minute") }} as actual, 1 as expected from sysibm.sysdummy1
+union all select {{ datediff("'1999-12-31 23:00:00.000000'", "'2000-01-01 00:00:00.000000'", "hour") }} as actual, 1 as expected from sysibm.sysdummy1
+union all select {{ datediff("'1999-12-31 00:00:00.000000'", "'2000-01-01 00:00:00.000000'", "day") }} as actual, 1 as expected from sysibm.sysdummy1
+union all select {{ datediff("'1999-12-26 00:00:00.000000'", "'2000-01-03 00:00:00.000000'", "week") }} as actual, 1 as expected from sysibm.sysdummy1
+union all select {{ datediff("'1999-12-01 00:00:00.000000'", "'2000-01-01 00:00:00.000000'", "month") }} as actual, 1 as expected from sysibm.sysdummy1
+union all select {{ datediff("'1999-10-01 00:00:00.000000'", "'2000-01-01 00:00:00.000000'", "quarter") }} as actual, 1 as expected from sysibm.sysdummy1
+union all select {{ datediff("'1999-01-01 00:00:00.000000'", "'2000-01-01 00:00:00.000000'", "year") }} as actual, 1 as expected from sysibm.sysdummy1
+"""
+
+models__test_datediff_yml = """
+version: 2
+models:
+  - name: test_datediff
+    tests:
+      - assert_equal:
+          actual: actual
+          expected: expected
+"""
+
+class TestHashIBMDB2(BaseHash):
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {
+            "data_hash.csv": seeds__data_hash_csv,
+            "data_hash.yml": seeds__schema_hash_yml,
+        }
+
+class TestPositionIBMDB2(BasePosition):
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {
+            "data_position.csv": seeds__data_position_csv,
+            "data_position.yml": seeds__schema_position_yml,
+        }
+
+class TestLastDayIBMDB2(BaseLastDay):
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {
+            "data_last_day.csv": seeds__data_last_day_csv,
+            "data_last_day.yml": seeds__schema_last_day_yml,
+        }
+
+class TestTypeStringIBMDB2(BaseTypeString):
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {
+            "expected.csv": seeds__data_type_string_csv,
+            "expected.yml": seeds__schema_type_string_yml,
+        }
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {"actual.sql": self.interpolate_macro_namespace(models__type_string_actual_sql, "type_string")}
+
+class TestSplitPartIBMDB2(BaseSplitPart):
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {
+            "data_split_part.csv": seeds__data_split_part_csv,
+            "data_split_part.yml": seeds__schema_split_part_yml,
+        }
+
+class TestDateAdd(BaseDateAdd):
+    pass
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {"data_dateadd.csv": seeds__data_dateadd_csv}
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "test_dateadd.csv": seeds__data_dateadd_csv,
+            "test_dateadd.yml": seeds__schema_dateadd_yml,
+            "test_dateadd.sql": self.interpolate_macro_namespace(
+                models__test_dateadd_sql, "dateadd"
+            ),
+        }
+
+class TestDateDiff(BaseDateDiff):
+    pass
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {
+            "data_datediff.csv": seeds__data_datediff_csv,
+            "data_datediff.yml": seeds__schema_datediff_yml,
+        }
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "test_datediff.yml": models__test_datediff_yml,
+            "test_datediff.sql": self.interpolate_macro_namespace(
+                models__test_datediff_sql, "datediff"
+            ),
+        }
