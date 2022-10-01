789b5b68526272acbcefa1c0cdf8446ca6c35ee3
aurany <rasmus.nyberg@gmail.com>
2022-10-01 22:04:33 +0200

    Added db2 specific show grants sql template code

--- dbt/include/ibmdb2/macros/adapters.sql
+++ dbt/include/ibmdb2/macros/adapters.sql
@@ -267,3 +267,20 @@ CURRENT TIMESTAMP - CURRENT TIMEZONE
     {% do adapter.truncate_relation(staging_relation) %}
     {% do adapter.drop_relation(staging_relation) %}
 {% endmacro %}
+
+
+{% macro ibmdb2__get_show_grant_sql(relation) %}
+
+  {%- set schema = case_relation_part(relation.quote_policy['schema'], relation.schema) -%}
+  {%- set identifier = case_relation_part(relation.quote_policy['identifier'], relation.identifier) -%}
+
+  SELECT
+    AUTHID as "grantee",
+    PRIVILEGE as "privilege_type"
+  FROM SYSIBMADM.PRIVILEGES
+  WHERE OBJECTNAME = ' {{ identifier }}'
+  {% if relation.schema %}
+    AND OBJECTSCHEMA = '{{ schema }}'
+  {% endif %}
+
+{% endmacro %}
