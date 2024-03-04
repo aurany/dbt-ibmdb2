0291775576017e2de7b0c041340c8fb4a4cb099d
Niels Pardon <par@zurich.ibm.com>
2024-03-04 08:59:47 -0500

    Fix split_part macro
    Signed-off-by: Niels Pardon <par@zurich.ibm.com>

--- dbt/include/ibmdb2/macros/utils/split_part.sql
+++ dbt/include/ibmdb2/macros/utils/split_part.sql
@@ -1,5 +1,14 @@
 {% macro ibmdb2__split_part(string_text, delimiter_text, part_number) %}
 
-    REGEXP_SUBSTR('{{ string_text }}', '([^{{ delimiter_text }}]*){{ delimiter_text }}?', 1, {{ part_number }}, '', 1)
+  {% if part_number == 0 %}
+    {{ exceptions.raise_compiler_error("Invalid argument `part_number` must not be 0.") }}
+  {% elif part_number >= 1 %}
+    REGEXP_SUBSTR({{ string_text }}, '([^' || REGEXP_REPLACE({{ delimiter_text }}, '([\|.^$\\+?*\[\]])', '\\$1') || ']*)' || REGEXP_REPLACE({{ delimiter_text }}, '([\|.^$\\+?*\[\]])', '\\$1') || '?', 1, {{ part_number }}, '', 1)
+  {% else %}
+    CASE
+      WHEN REGEXP_COUNT({{ string_text }}, '([^' || REGEXP_REPLACE({{ delimiter_text }}, '([\|.^$\\+?*\[\]])', '\\$1') || ']*)' || REGEXP_REPLACE({{ delimiter_text }}, '([\|.^$\\+?*\[\]])', '\\$1') || '?') > 0 THEN REGEXP_SUBSTR({{ string_text }}, '([^' || REGEXP_REPLACE({{ delimiter_text }}, '([\|.^$\\+?*\[\]])', '\\$1') || ']*)' || REGEXP_REPLACE({{ delimiter_text }}, '([\|.^$\\+?*\[\]])', '\\$1') || '?', 1, REGEXP_COUNT({{ string_text }}, '([^' || REGEXP_REPLACE({{ delimiter_text }}, '([\|.^$\\+?*\[\]])', '\\$1') || ']*)' || REGEXP_REPLACE({{ delimiter_text }}, '([\|.^$\\+?*\[\]])', '\\$1') || '?') + {{ part_number }}, '', 1)
+      ELSE NULL
+    END
+  {% endif %}
 
 {% endmacro %}
